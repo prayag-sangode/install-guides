5b2e8fc8b9bcd34f9ac92c0c2ef07eb0f8698d65
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-05-29 16:14:41 +0530

    Create docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+which docker-compose
+docker-compose --version
