3439affa3c5a33c525c595a8f9a7ac5caa0d783a
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-06-05 23:51:21 +0530

    Update docker-rocky8.sh

--- docker-rocky8.sh
+++ docker-rocky8.sh
@@ -4,4 +4,6 @@
 sudo yum -y install yum-utils
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install -y docker-ce
+sudo usermod -aG docker $USER
 which docker
+docker ps
