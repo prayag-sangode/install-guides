8d0594ce31a80e500bca6e18a01b13e5f51f248b
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-06-08 19:43:31 +0530

    Update azcopy-install.sh

--- azcopy-install.sh
+++ azcopy-install.sh
@@ -3,5 +3,5 @@
 #
 wget https://aka.ms/downloadazcopy-v10-linux
 tar -xvf downloadazcopy-v10-linux
-sudo cp ./azcopy_linux_amd64_*/azcopy
+sudo cp ./azcopy_linux_amd64_*/azcopy /usr/local/bin
 which azcopy
