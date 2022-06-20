410762cb2d4e4619e4663e3f72e9c398d0e9d0a6
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-06-20 22:19:50 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,3 +1,5 @@
+
+sudo yum -y install php-gd php-soap php-intl php-mysqlnd php-pdo php-pecl-zip php-fpm php-opcache php-curl php-zip php-xmlrpc wget
 sudo curl https://wordpress.org/latest.tar.gz -o /var/www/html
 sudo tar zxvf /var/www/html/latest.tar.gz -C /var/www/html/
 sudo chown -R apache:apache /var/www/html/wordpress
