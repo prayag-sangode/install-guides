d6d845c904d975780839cef7def3755fe2029eab
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-06-20 22:06:38 +0530

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -3,8 +3,8 @@
 # Install LAPM stack on RockyLinux8/RHEL8
 sudo yum -y install httpd 
 sudo systemctl enable --now httpd
-sudo firewall-cmd --permanent --zone=pulic --add-service=http
-sudo firewall-cmd --permanent --zone=pulic --add-service=https
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-service=https
 sudo firewall-cmd --reload
 sudo yum -y install php php-mysqlnd php-pdo php-gd php-mbstring
 sudo yum -y install mariadb-server mariadb
