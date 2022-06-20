6474b53fabd181022cfed6b30393cd640dca2d03
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-06-20 22:04:13 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,2 +1,4 @@
 sudo curl https://wordpress.org/latest.tar.gz -o /var/www/html
 sudo tar zxvf /var/www/html/latest.tar.gz -C /var/www/html/
+sudo chown -R apache:apache /var/www/html/wordpress
+sudo ls -ld /var/www/html/wordpress
