526fcf00f0318ee1b108a04e39c772963b8c9be8
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-07-12 17:28:26 +0530

    Update aws-install.sh

--- aws-install.sh
+++ aws-install.sh
@@ -4,5 +4,5 @@
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 mkdir ~/aws-cli-install
 unzip awscli-bundle.zip -d aws-cli-install
-sudo ./~aws-cli-install/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+sudo ~/aws-cli-install/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 which aws
