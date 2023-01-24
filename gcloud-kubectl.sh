d8f2534ebd1a58d3404c8b41d639f6a021aacdcc
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-24 23:43:14 +0530

    Update gcloud-kubectl.sh

--- gcloud-kubectl.sh
+++ gcloud-kubectl.sh
@@ -4,6 +4,7 @@
 curl https://sdk.cloud.google.com | bash -s -- --disable-prompts
 export PATH=$PATH:~/google-cloud-sdk/bin
 which gcloud
+gcloud auth application-default login
 gcloud components install kubectl
 which kubectl
 
