f6a462a4a1165096998cd3ccb042cb55e5bfe9b3
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-24 23:45:16 +0530

    Create bmctl.sh

---
+++ bmctl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author : Prayag Sangode
+#
+curl https://sdk.cloud.google.com | bash -s -- --disable-prompts
+export PATH=$PATH:~/google-cloud-sdk/bin
+which gcloud
+gcloud auth application-default login
+gcloud components install kubectl
+which kubectl
+export VERSION=1.14.0
+gsutil cp gs://anthos-baremetal-release/bmctl/$VERSION/linux-amd64/bmctl .
+chmod +x ./bmctl
+which bmctl
+gsutil cp gs://anthos-baremetal-release/bmctl/$VERSION/linux-amd64/bmpackages_$VERSION.tar.xz .
