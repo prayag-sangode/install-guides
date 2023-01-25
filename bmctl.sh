24f16520ed8d65e4b230ce93c8ac1c8fa977ee31
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-25 13:55:23 +0530

    Update bmctl.sh

--- bmctl.sh
+++ bmctl.sh
@@ -12,5 +12,4 @@ gcloud config set project $PROJECT_ID
 export VERSION=1.14.0
 gsutil cp gs://anthos-baremetal-release/bmctl/$VERSION/linux-amd64/bmctl .
 chmod +x ./bmctl
-which bmctl
 gsutil cp gs://anthos-baremetal-release/bmctl/$VERSION/linux-amd64/bmpackages_$VERSION.tar.xz .
